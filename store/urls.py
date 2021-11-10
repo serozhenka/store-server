5455a97f50fceb5a5bc75b9de7861b72b88ad212
seerozhenkaa <sergeykaoff@gmail.com>
2021-11-10 21:20:51 +0200

    Added urls

--- store/urls.py
+++ store/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('products/', views.products, name='products')
 ]
