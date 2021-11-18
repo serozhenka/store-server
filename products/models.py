3f37475a7d3c4147f22095d28e3a9793564a9906
seerozhenkaa <sergeykaoff@gmail.com>
2021-11-18 21:04:14 +0200

    Added Basket

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import User
 
 
 class ProductCategory(models.Model):
@@ -22,5 +23,17 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name} | {self.category.name}'
+
+class Basket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+    created_timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Basket for {self.user.username} | Product {self.product.name}'
+
+    def sum(self):
+        return self.quantity * self.product.price
 
 
